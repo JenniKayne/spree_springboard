9baa9c5fc88a9f798f1db91334c5abc337d39bee
truenito <gabriel@estilopanda.com>
2019-08-27 10:14:06 -0400

    Preventing errors for already running jobs.

--- app/jobs/spree_springboard/update_gift_card_job.rb
+++ app/jobs/spree_springboard/update_gift_card_job.rb
@@ -3,11 +3,15 @@ module SpreeSpringboard
     queue_as :springboard
 
     def perform(gift_card_id)
-      gift_card = Spree::GiftCard.find(gift_card_id)
+      gift_card = if gift_card_id.is_a? Integer
+                    Spree::GiftCard.find(gift_card_id)
+                  else
+                    gift_card_id
+                  end
 
       gift_card.springboard_adjust_balance!
     rescue StandardError => e
-      Raven.extra_context(updated_gift_card: gift_card.code)
+      Raven.extra_context(gift_card: gift_card, updated_gift_card: gift_card&.code)
 
       raise e
     end
